f4c8be6eed9db54f3e95055f57187cf9fc94187d
choi <cjy1749@naver.com>
2025-10-10 16:25:48 +0900

    10-10 commit

---
+++ react/self-react/src/components/commons/Footer.jsx
@@ -0,0 +1,7 @@
+export function Footer () {
+    return (
+        <footer>
+            <h1>푸터</h1>
+        </footer>
+    );
+}
---
+++ react/self-react/src/pages/Home.jsx
@@ -0,0 +1,7 @@
+export function Home () {
+    return(
+        <div className="home">
+         <h1>홈</h1>
+        </div>
+    )
+}
